56d53ba244744c017dedf72b23fca22e4e4dbc38
Rohit Yadav <rohityadav876@gmail.com>
2024-09-04 17:22:03 +0530

    Adds single number edge case and basic two number sum test and implementation

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -5,4 +5,12 @@ describe('StringCalculator Function Test Suite', () => {
     const calculator = new StringCalculator();
     expect(calculator.add('')).toBe(0);
   });
+  it('should return the number itself for a single number', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('1')).toBe(1);
+  });
+  it('should return the sum of two comma-separated numbers', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('1,5')).toBe(6);
+  });
 });
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 class StringCalculator {
   add(numbers) {
     if (!numbers) return 0;
-    return 0;
+    const numberArray = numbers.split(',');
+    return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
 
